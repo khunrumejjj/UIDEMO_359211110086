53a36916b84120007353b08f5f73380c68c893cc
khunrumejjj <khunrumejjj@gmail.com>
2018-02-14 11:39:55 +0700

    dbConnection

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
     private loginModel model = new loginModel();
-    //import object from scence builder
+
+    //import object form scene builder
     @FXML
     private Label dbStatus;
+
     @FXML
     private Label loginStatus;
     @FXML
@@ -25,13 +29,33 @@ public class Controller implements Initializable{
     private Button btnLogin;
 
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if (this.model.isDatabaseConnection()){
+        if (this.model.isDatabaseConnection()) {
             this.dbStatus.setText("Connected to DB");
-        }else {
+
+        } else {
             this.dbStatus.setText("Not Connect to DB");
+
         }
 
-    }
-}//class
+    }//intialize
+
+    @FXML
+    public void Login(ActionEvent event) {
+//        System.out.println(username.getText());
+//        System.out.println(password.getText());
+        try {
+            if (this.model.isLogin(username.getText(), password.getText())) {
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+//                stage.close();
+                loginStatus.setText(" Welcome to Our System ");
+            } else {
+                loginStatus.setText("Your username or password is invalid");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }//login
+}//Class
